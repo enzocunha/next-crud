84ecd4908709f0fd9cb0a297b9638d7a3d4d87bd
enzocunha <enzcdn@discente.ufg.br>
2023-04-26 15:49:54 -0300

    get, put and delete by id for book and author

--- pages/api/author/[id].js
+++ pages/api/author/[id].js
@@ -1,7 +1,56 @@
-export default function handler(req, res) {
+import dbConnect from '@/lib/dbConnect';
+import Author from '@/models/authorModel';
+
+export default async function handler(req, res) {
 	const {
 		query: { id },
 		method,
 	} = req;
-	res.status(200).json({ name: `${id}` });
+
+	await dbConnect();
+
+	switch (method) {
+		case 'GET' /* Get a model by its ID */:
+			try {
+				const author = await Author.findById(id);
+				if (!author) {
+					return res.status(400).json({ success: false });
+				}
+				res.status(200).json({ success: true, data: author });
+			} catch (error) {
+				res.status(400).json({ success: false });
+			}
+			break;
+
+		case 'PUT' /* Edit a model by its ID */:
+			try {
+				const author = await Author.findByIdAndUpdate(id, req.body, {
+					new: true,
+					runValidators: true,
+				});
+				if (!author) {
+					return res.status(400).json({ success: false });
+				}
+				res.status(200).json({ success: true, data: author });
+			} catch (error) {
+				res.status(400).json({ success: false });
+			}
+			break;
+
+		case 'DELETE' /* Delete a model by its ID */:
+			try {
+				const deletedAuthor = await Author.deleteOne({ _id: id });
+				if (!deletedAuthor) {
+					return res.status(400).json({ success: false });
+				}
+				res.status(200).json({ success: true, data: {} });
+			} catch (error) {
+				res.status(400).json({ success: false });
+			}
+			break;
+
+		default:
+			res.status(400).json({ success: false });
+			break;
+	}
 }
