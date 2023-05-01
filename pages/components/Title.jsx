9d901d9cf77d17847b97fc3027ae2439c0c22128
enzocunha <enzcdn@discente.ufg.br>
2023-05-01 15:36:05 -0300

    changing title style

--- pages/components/Title.jsx
+++ pages/components/Title.jsx
@@ -1,7 +1,7 @@
 function Title({ title }) {
 	return (
 		<div className='mx-auto py-12 max-w-2xl text-center'>
-			<h2 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl dark:text-gray-400'>
+			<h2 className='text-3xl tracking-tight text-gray-700 sm:text-4xl dark:text-gray-400'>
 				{title}
 			</h2>
 		</div>
