898336cb19e209d7eb8f4cf2e16164af3dccaba8
enzocunha <enzcdn@discente.ufg.br>
2023-05-01 14:34:46 -0300

    title dark mode text color

--- pages/components/Title.jsx
+++ pages/components/Title.jsx
@@ -1,7 +1,7 @@
 function Title({ title }) {
 	return (
 		<div className='mx-auto py-12 max-w-2xl text-center'>
-			<h2 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl dark:text-white'>
+			<h2 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl dark:text-gray-400'>
 				{title}
 			</h2>
 		</div>
