07a47178fb38faa9d42d8211d642ae0a715ba424
enzocunha <enzcdn@discente.ufg.br>
2023-05-02 11:18:56 -0300

    added page not found

---
+++ pages/404.jsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+
+function NotFound() {
+	return (
+		<div className='flex flex-col items-center justify-center h-screen'>
+			<h1 className='text-4xl font-bold mb-4'>404 - Page Not Found</h1>
+			<p className='text-lg mb-4'>
+				Sorry, the page you are looking for does not exist.
+			</p>
+			<Link href='/' className='text-blue-500 hover:underline'>
+				Go back
+			</Link>
+		</div>
+	);
+}
+
+export default NotFound;
